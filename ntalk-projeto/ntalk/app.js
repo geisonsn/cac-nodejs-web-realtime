51821d22e206817f7afc3bf4cd87c5dd1580c684
= <geison.sn@gmail.com>
2015-05-19 16:58:05 -0400

    Implementação de CRUD de usuários

--- ntalk-projeto/ntalk/app.js
+++ ntalk-projeto/ntalk/app.js
@@ -1,14 +1,24 @@
 var express = require("express"),
-	routes = require("./routes/index"), 
-	users = require("./routes/users"), 
+	load = require("express-load"),
+	bodyParser = require("body-parser"),
+	cookieParser = require("cookie-parser"),
+	expressSession = require("express-session"),
+	methodOverride = require("method-override"),
 	app = express();
 
 app.set("views", __dirname + "/views");
 app.set("view engine", "ejs");
+app.use(cookieParser("ntalk"));
+app.use(expressSession());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(methodOverride("_method"));
 app.use(express.static(__dirname + "/public"));
 
-app.use("/", routes);
-app.use("/usuarios", users);
+load("models")
+	.then("controllers")
+	.then("routes")
+	.into(app);
 
 app.listen(3000, function() {
 	console.log("Ntalk no ar.");
